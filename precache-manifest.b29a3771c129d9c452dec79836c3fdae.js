256a5ebe04b90f4b22bb2c47dbdd15cf1a95ee77
cgarcia0 <cgarcia0@stanford.edu>
2022-08-26 17:46:47 -0700

    Updates

---
+++ precache-manifest.b29a3771c129d9c452dec79836c3fdae.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae60ce12af3f008b5c461f021f8dddd9",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "2363138694fe53761a2d",
+    "url": "/my-website/static/css/main.16704afd.chunk.css"
+  },
+  {
+    "revision": "b211e55899ae2536788e",
+    "url": "/my-website/static/js/2.9c7104c2.chunk.js"
+  },
+  {
+    "revision": "0c91d6cf6408089b81f706ebde51ebe6",
+    "url": "/my-website/static/js/2.9c7104c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2363138694fe53761a2d",
+    "url": "/my-website/static/js/main.1faca45c.chunk.js"
+  },
+  {
+    "revision": "70e3aeffcd68fa4813da",
+    "url": "/my-website/static/js/runtime-main.6f3c7d96.js"
+  },
+  {
+    "revision": "187d652100dd5108830a1d82ddddcd32",
+    "url": "/my-website/static/media/CristobalGarcia.187d6521.png"
+  },
+  {
+    "revision": "4c0bbfa638dd2bfacca06c6496176cea",
+    "url": "/my-website/static/media/arduino1_filter.4c0bbfa6.jpg"
+  },
+  {
+    "revision": "95ec0ed07f747b1b23022f1f17b971b5",
+    "url": "/my-website/static/media/arduino2.95ec0ed0.mp4"
+  },
+  {
+    "revision": "2323166064cc21284568e2f5bd34aef9",
+    "url": "/my-website/static/media/arduino3.23231660.mp4"
+  },
+  {
+    "revision": "988699f29b798ef4fa57ba7f19736987",
+    "url": "/my-website/static/media/arduino4.988699f2.mp4"
+  },
+  {
+    "revision": "5dde2594dd1023fa17b6b7d296f5e551",
+    "url": "/my-website/static/media/arduinoCircuit.5dde2594.png"
+  },
+  {
+    "revision": "6eb61aa6bb9298a565e254d4d71f3ca6",
+    "url": "/my-website/static/media/arduinoThumbNail1.6eb61aa6.png"
+  },
+  {
+    "revision": "345c82f490416715ece0afb0e9aa2b3d",
+    "url": "/my-website/static/media/arduinoThumbNail2.345c82f4.jpg"
+  },
+  {
+    "revision": "05c040a3d9a76d7ee8d9431b18bd6dde",
+    "url": "/my-website/static/media/arduinoThumbNail33.05c040a3.png"
+  },
+  {
+    "revision": "715b352c2e57edcb8e0be1be7c9358bc",
+    "url": "/my-website/static/media/lightController.715b352c.mp4"
+  },
+  {
+    "revision": "2eb6646b8d47ef5a4ea85a3d8e9dd275",
+    "url": "/my-website/static/media/lightController1.2eb6646b.png"
+  },
+  {
+    "revision": "d0aadd3bffe41347d857da212736384d",
+    "url": "/my-website/static/media/lightControllerCode.d0aadd3b.png"
+  },
+  {
+    "revision": "2b686f2e5682570fb85fc952a4581419",
+    "url": "/my-website/static/media/logo.2b686f2e.svg"
+  },
+  {
+    "revision": "413d8e27145c3f0a8c694b97c1b49d35",
+    "url": "/my-website/static/media/pexels-bg1.413d8e27.jpg"
+  },
+  {
+    "revision": "04aa54435cd2d87a8da6683eae9ff71d",
+    "url": "/my-website/static/media/pexels-pixabay-459277.04aa5443.jpg"
+  },
+  {
+    "revision": "79de9e00ffd46bf00b4b09766a4694a2",
+    "url": "/my-website/static/media/pixabay-blog1.79de9e00.jpg"
+  },
+  {
+    "revision": "d6ee9f574fc61b22a6b203528439595c",
+    "url": "/my-website/static/media/pixabay-chip1.d6ee9f57.jpg"
+  },
+  {
+    "revision": "6998f7607c6b0ccd0f68d0e71c10ccba",
+    "url": "/my-website/static/media/transistors.6998f760.jpg"
+  }
+]);
